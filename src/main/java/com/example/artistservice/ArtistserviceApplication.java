d65f08a0f5de89b5e9e0983b7902b78742a952d8
Jolien Foets <57670042+JolienFoets@users.noreply.github.com>
2021-10-01 11:04:16 +0200

    init

---
+++ src/main/java/com/example/artistservice/ArtistserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.artistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtistserviceApplication.class, args);
+    }
+
+}
