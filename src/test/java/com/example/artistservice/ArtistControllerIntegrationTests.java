f1db890548277369b6934056e18dee2ff398d102
Jolien Foets <57670042+JolienFoets@users.noreply.github.com>
2021-12-30 14:06:30 +0100

    Changes UnitTest and IntegrationTest

--- src/test/java/com/example/artistservice/ArtistControllerIntegrationTests.java
+++ src/test/java/com/example/artistservice/ArtistControllerIntegrationTests.java
@@ -41,7 +41,7 @@ public class ArtistControllerIntegrationTests {
     private Artist artistArtist1Album1 = new Artist("1", 001, "Justin Bieber", "0623964", 5);
     private Artist artistArtist1Album2 = new Artist("1", 001, "Justin Bieber", "0623964", 10);
     private Artist artistArtist2Album1 = new Artist("2", 002, "Shawn Mendes", "3956297", 4);
-    private Artist artistToBeDeleted = new Artist("99", 99, "Lady Gaga", "4167318", 6);
+    private Artist artistToBeDeleted = new Artist("99", 999, "Lady Gaga", "4167318", 6);
 
     @BeforeEach
     public void beforeAllTests() {
@@ -59,64 +59,30 @@ public class ArtistControllerIntegrationTests {
 
     private ObjectMapper mapper = new ObjectMapper();
 
-    @Test
-    public void givenArtist_whenGetArtistByArtistIdAndMBID_thenReturnJsonArtist() throws Exception {
-        mockMvc.perform(get("/artists/{artistId}/{MBID}", 001, "0623964"))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.artistId", is(001)))
-                .andExpect(jsonPath("$.MBID", is("0623964")))
-                .andExpect(jsonPath("$.numberStreams", is(5)));
-    }
 
-    @Test
-    public void givenArtist_whenGetArtistsByMBID_thenReturnJsonArtists() throws Exception {
-        List<Artist> artistList = new ArrayList<>();
-        artistList.add(artistArtist1Album1);
-        artistList.add(artistArtist2Album1);
-
-        mockMvc.perform(get("/artists/{MBID}", "0623964"))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].artistId", is(001)))
-                .andExpect(jsonPath("$[0].MBID", is("0623964")))
-                .andExpect(jsonPath("$[0].numberStreams", is(5)))
-                .andExpect(jsonPath("$[1].artistId", is(002)))
-                .andExpect(jsonPath("$[1].MBID", is("3956297")))
-                .andExpect(jsonPath("$[1].numberStreams", is(4)));
-
-    }
 
     @Test
     public void givenArtist_whenGetArtistsByArtistId_thenReturnJsonArtists() throws Exception {
-        List<Artist> artistList = new ArrayList<>();
-        artistList.add(artistArtist1Album1);
-        artistList.add(artistArtist1Album2);
 
-        mockMvc.perform(get("/artists/{artistId}", 001))
+        mockMvc.perform(get("/api/artists/{artistId}", 001))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].artistId", is(001)))
-                .andExpect(jsonPath("$[0].MBID", is("0623964")))
-                .andExpect(jsonPath("$[0].numberStreams", is(5)))
-                .andExpect(jsonPath("$[1].artistId", is(001)))
-                .andExpect(jsonPath("$[1].MBID", is("0623964")))
-                .andExpect(jsonPath("$[1].numberStreams", is(10)));
+                .andExpect(jsonPath("$.artistId", is(001)))
+                .andExpect(jsonPath("$.mbid", is("0623964")))
+                .andExpect(jsonPath("$.numberStreams", is(10)));
     }
 
     @Test
     public void whenPostArtist_thenReturnJsonArtist() throws Exception {
         Artist artistArtist3Album1 = new Artist("3", 003, "Katy Perry", "5178419", 10);
 
-        mockMvc.perform(post("/artists")
+        mockMvc.perform(post("/api/artists")
                 .content(mapper.writeValueAsString(artistArtist3Album1))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.artistId", is(003)))
-                .andExpect(jsonPath("$.MBID", is("5178419")))
+                .andExpect(jsonPath("$.mbid", is("5178419")))
                 .andExpect(jsonPath("$.numberStreams", is(10)));
 
     }
@@ -125,20 +91,20 @@ public class ArtistControllerIntegrationTests {
     public void givenArtist_whenPutArtist_thenReturnJsonArtist() throws Exception {
         Artist updatedArtist = new Artist("1", 001, "Justin Bieber", "0623964", 9);
 
-        mockMvc.perform(put("/artists")
+        mockMvc.perform(put("/api/artists/{artistId}", 001)
                 .content(mapper.writeValueAsString(updatedArtist))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.artistId", is(001)))
-                .andExpect(jsonPath("$.MBID", is("0623964")))
+                .andExpect(jsonPath("$.mbid", is("0623964")))
                 .andExpect(jsonPath("$.numberStreams", is(9)));
     }
 
     @Test
     public void givenArtist_whenDeleteArtist_thenStatusOk() throws Exception {
 
-        mockMvc.perform(delete("/artists/{artistId}", 999)
+        mockMvc.perform(delete("/api/artists/{artistId}", 999)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
@@ -146,7 +112,7 @@ public class ArtistControllerIntegrationTests {
     @Test
     public void givenNoArtist_whenDeleteArtist_thenStatusNotFound() throws Exception {
 
-        mockMvc.perform(delete("/artists/{artistId}", 888)
+        mockMvc.perform(delete("/api/artists/{artistId}", 888)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
     }
